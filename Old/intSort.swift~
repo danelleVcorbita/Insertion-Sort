fde55ceb98e0d96c90514f0b4e475d197167c6dc
Danelle Corbita <danelle.corbita@gmail.com>
2022-10-07 15:00:00 -0500

    setup

---
+++ Old/intSort.swift~
@@ -0,0 +1,27 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let unsortedIntegers = [5, 1, 4, 2, 8]
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+var array = unsortedIntegers
+var x = 0
+var swap = 0
+var total = 0
+for num in 0..<array.count {
+    var n = num
+    while n > 0 && array[n] < array[n-1] {
+        x = array[n]
+        array[n] = array[n-1]
+        array[n-1] = x
+        swap += 1
+        total += 1
+        n -= 1
+    }
+    print("Pass: \(num), Swaps: \(swap)/\(total), Array: \(array)")
+    swap = 0
+}
--- main.swift
+++ main.swift
@@ -1,27 +1,41 @@
-
-
-// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-let unsortedIntegers = [5, 1, 4, 2, 8]
-// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
-// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
-
-
-// Add your code below:
-var array = unsortedIntegers
-var x = 0
+import Foundation
+var unsortedString = [""]
+var unsortedIntegers: [Int] = []
+while let input = readLine() {
+    unsortedString.append(input)
+}
+var a = 0
+var b = 0
+var x:Float = 0
+for _ in unsortedString {
+    for w in unsortedString[a].utf8 {
+        x += Float(w)/pow((1000.0), Float(b))
+        b += 1
+    }
+    unsortedIntegers.append(Int(x))
+    b = 0
+    a += 1
+    x = 0
+}
+var unInt = unsortedIntegers
+var unString = unsortedString
+var z = 0
 var swap = 0
-var total = 0
-for num in 0..<array.count {
-    var n = num
-    while n > 0 && array[n] < array[n-1] {
-        x = array[n]
-        array[n] = array[n-1]
-        array[n-1] = x
+var total = 0 
+for value in 0..<unInt.count {
+    var n = value
+    while n > 0 && unInt[n] < unInt[n-1] {
+        z = unInt[n]
+        unInt[n] = unInt[n-1]
+        unInt[n-1] = z
+        let largerString = unString[n-1]        
+        unString[n-1] = unString[n]
+        unString[n] = largerString
+        
         swap += 1
         total += 1
-        n -= 1
+        n -= 1        
     }
-    print("Pass: \(num), Swaps: \(swap)/\(total), Array: \(array)")
     swap = 0
 }
+print(unInt, unString)
